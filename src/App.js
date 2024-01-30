a729bc10733c289a90696effca43643375b6f117
liam <liambialy2@gmail.com>
2024-01-30 19:32:07 -0300

    fix points errors

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const handleClick = (e) => {
 
     const controlZeros = /[+\-/*]0{1,}$/;
+    const controlPoints = /[+\-/*]|\d*\.\d*$/;
 
     if (result !== 'Error' && result !== '0') {
         //controlamos que al obtener el error NaN se modifique por el numero ingresado
@@ -16,24 +17,45 @@ function App() {
           setResult(e.target.name);
         }
         //controlamos que el final de la cadena sea un operador o un punto
-        if (result.endsWith('+') || result.endsWith('-') || result.endsWith('/') || result.endsWith('*') || result.endsWith('.')) {
+        if (result.endsWith('+') || result.endsWith('/') || result.endsWith('*') || result.endsWith('-')) {
           
-          if ((e.target.name !== '+') && (e.target.name !== '/') && (e.target.name !== '*') && (e.target.name !== '.')) {
+          if ((e.target.name !== '+') && (e.target.name !== '/') && (e.target.name !== '*')) {
             
-            if (result.endsWith('-') && (e.target.name === '-')) {
+            if (e.target.name === '-' && result.endsWith('-')) {
+              // setResult(result.slice(0, -1) + e.target.name)
               setResult(result.concat(''));
             } else {
               setResult(result.concat(e.target.name));
             }
           } else {
-            setResult(result.concat(''))
+            if ((e.target.name === '+') || (e.target.name === '/') || (e.target.name === '*') || (e.target.name === '.')) {
+              setResult(result.slice(0, -1) + e.target.name)
+            } else {
+              if (e.target.name === '-') {
+                setResult(result.concat(e.target.name))
+              } else {
+                setResult(result.concat(''))
+              }
+            }
           }
         } else{
           //controlamos que no se ingresen mas de un 0 como numero entre operaciones
           if (controlZeros.test(result) && e.target.classList.contains('numbers')){
             setResult(result.concat(''))
           } else{
-            setResult(result.concat(e.target.name));
+            //controlamos que se ingrese solamente un punto por numero
+            if (controlPoints.test(result) && e.target.name === '.') {
+              setResult(result.concat(''));
+            } else {
+
+              if (result.endsWith('-') && (e.target.name === '-')) {
+                setResult(result.concat(''));
+              } else {
+                setResult(result.concat(e.target.name));
+
+                
+              }
+            }
           }
         }
         //manejo de errores y controlar que al principio solo se ingresen numeros o un signo -
@@ -55,8 +77,12 @@ function App() {
     }
   }
 
-  const clear = () => {
+  const Allclear = () => {
     setResult("");
+  }
+
+  const clear = () => {
+    setResult(result.slice(0, -1))
   }
 
   const calculate = () => {
@@ -68,7 +94,7 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <div className="container bg-secondary p-5">
       <InputGroup className="mb-3">
         <FormControl
           placeholder="0"
@@ -77,24 +103,25 @@ function App() {
           id='display'
         />
       </InputGroup>
-      <div className="button">
-        <Button variant="danger" onClick={clear} id="clear">Clear</Button>
-        <Button name="/" className='operators' variant="info" onClick={handleClick} id="divide">/</Button>
-        <Button name="7" className='numbers' onClick={handleClick} id='seven'>7</Button>
-        <Button name="8" className='numbers' onClick={handleClick} id='eight'>8</Button>
-        <Button name="9" className='numbers' onClick={handleClick} id='nine'>9</Button>
-        <Button name="*" className='operators' variant="info" onClick={handleClick} id='multiply'>*</Button>
-        <Button name="4" className='numbers' onClick={handleClick} id='four'>4</Button>
-        <Button name="5" className='numbers' onClick={handleClick} id='five'>5</Button>
-        <Button name="6" className='numbers' onClick={handleClick} id='six'>6</Button>
-        <Button name="-" className='operators' variant="info" onClick={handleClick} id='subtract'>-</Button>
-        <Button name="1" className='numbers' onClick={handleClick} id='one'>1</Button>
-        <Button name="2" className='numbers' onClick={handleClick} id='two'>2</Button>
-        <Button name="3" className='numbers' onClick={handleClick} id='three'>3</Button>
-        <Button name="+" className='operators' variant="info" onClick={handleClick} id='add'>+</Button>
-        <Button name="0" className='numbers' onClick={handleClick} id='zero'>0</Button>
-        <Button name="." onClick={handleClick} id='decimal'>.</Button>
-        <Button variant="primary" onClick={calculate} id="equals">=</Button>
+      <div className="button row row-cols-4 gap-1">
+        <Button variant="danger" className='col' onClick={Allclear} id="clear">AC</Button>
+        <Button variant="danger" className='col' onClick={clear} id="clear">C</Button>
+        <Button name="/" className='operators col' variant="info" onClick={handleClick} id="divide">/</Button>
+        <Button name="7" className='numbers col' onClick={handleClick} id='seven'>7</Button>
+        <Button name="8" className='numbers col' onClick={handleClick} id='eight'>8</Button>
+        <Button name="9" className='numbers col' onClick={handleClick} id='nine'>9</Button>
+        <Button name="*" className='operators col' variant="info" onClick={handleClick} id='multiply'>*</Button>
+        <Button name="4" className='numbers col' onClick={handleClick} id='four'>4</Button>
+        <Button name="5" className='numbers col' onClick={handleClick} id='five'>5</Button>
+        <Button name="6" className='numbers col' onClick={handleClick} id='six'>6</Button>
+        <Button name="-" className='operators col' variant="info" onClick={handleClick} id='subtract'>-</Button>
+        <Button name="1" className='numbers col' onClick={handleClick} id='one'>1</Button>
+        <Button name="2" className='numbers col' onClick={handleClick} id='two'>2</Button>
+        <Button name="3" className='numbers col' onClick={handleClick} id='three'>3</Button>
+        <Button name="+" className='operators col' variant="info" onClick={handleClick} id='add'>+</Button>
+        <Button name="0" className='numbers col' onClick={handleClick} id='zero'>0</Button>
+        <Button name="." className='col' onClick={handleClick} id='decimal'>.</Button>
+        <Button variant="primary" className='col' onClick={calculate} id="equals">=</Button>
       </div>
     </div>
   );
