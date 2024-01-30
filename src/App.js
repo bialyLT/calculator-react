1a45f49924d470b29ccb3a55991bdfdf8f1f42dc
liam <liambialy2@gmail.com>
2024-01-30 17:14:22 -0300

    fix zero init and other fixes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,46 @@ function App() {
   const [result, setResult] = useState("");
 
   const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
+
+    const controlZeros = /[+\-/*]0{2,}[+\-/*]/;
+
+    if (result !== 'Error' && result !== '0') {
+        if (isNaN(result)) {
+          setResult(e.target.name);
+        }
+        
+        if (result.endsWith('+') || result.endsWith('-') || result.endsWith('/') || result.endsWith('*') || result.endsWith('.')) {
+          
+          if ((e.target.name !== '+') && (e.target.name !== '/') && (e.target.name !== '*') && (e.target.name !== '.')) {
+            
+            if (result.endsWith('-') && (e.target.name === '-')) {
+              setResult(result.concat(''));
+            } else {
+              setResult(result.concat(e.target.name));
+            }
+          } else {
+            setResult(result.concat(''))
+          }
+        } else{
+          
+          setResult(result.concat(e.target.name));
+        }
+        //manejo de errores y controlar que al principio solo se ingresen numeros o un signo -
+      } else {
+        //controlamos los 0 del inicio
+      if ((e.target.name === '0')) {
+        setResult('');
+      } else {
+      if ((e.target.name === '+') || (e.target.name === '/') || (e.target.name === '*') || (e.target.name === '-')) {
+          setResult(result.concat(e.target.name));
+      } else {
+
+        //si aparece error o 0 al inicio simplemente modificamos por el numero elegido
+        setResult(e.target.name);
+      }
+        
+      }
+    }
   }
 
   const clear = () => {
@@ -16,10 +55,6 @@ function App() {
 
   const calculate = () => {
     try {
-
-      
-
-
       setResult(eval(result).toString());
     } catch(err) {
       setResult("Error");
@@ -33,26 +68,27 @@ function App() {
           placeholder="0"
           aria-label="Amount (to the nearest dollar)"
           value={result}
+          id='display'
         />
       </InputGroup>
       <div className="button">
-        <Button variant="danger" onClick={clear}>Clear</Button>
-        <Button name="/" variant="info" onClick={handleClick}>/</Button>
-        <Button name="7" onClick={handleClick}>7</Button>
-        <Button name="8" onClick={handleClick}>8</Button>
-        <Button name="9" onClick={handleClick}>9</Button>
-        <Button name="*" variant="info" onClick={handleClick}>*</Button>
-        <Button name="4" onClick={handleClick}>4</Button>
-        <Button name="5" onClick={handleClick}>5</Button>
-        <Button name="6" onClick={handleClick}>6</Button>
-        <Button name="-" variant="info" onClick={handleClick}>-</Button>
-        <Button name="1" onClick={handleClick}>1</Button>
-        <Button name="2" onClick={handleClick}>2</Button>
-        <Button name="3" onClick={handleClick}>3</Button>
-        <Button name="+" variant="info" onClick={handleClick}>+</Button>
-        <Button name="0" onClick={handleClick}>0</Button>
-        <Button name="." onClick={handleClick}>.</Button>
-        <Button variant="primary" onClick={calculate}>=</Button>
+        <Button variant="danger" onClick={clear} id="clear">Clear</Button>
+        <Button name="/" variant="info" onClick={handleClick} id="divide">/</Button>
+        <Button name="7" onClick={handleClick} id='seven'>7</Button>
+        <Button name="8" onClick={handleClick} id='eight'>8</Button>
+        <Button name="9" onClick={handleClick} id='nine'>9</Button>
+        <Button name="*" variant="info" onClick={handleClick} id='multiply'>*</Button>
+        <Button name="4" onClick={handleClick} id='four'>4</Button>
+        <Button name="5" onClick={handleClick} id='five'>5</Button>
+        <Button name="6" onClick={handleClick} id='six'>6</Button>
+        <Button name="-" variant="info" onClick={handleClick} id='subtract'>-</Button>
+        <Button name="1" onClick={handleClick} id='one'>1</Button>
+        <Button name="2" onClick={handleClick} id='two'>2</Button>
+        <Button name="3" onClick={handleClick} id='three'>3</Button>
+        <Button name="+" variant="info" onClick={handleClick} id='add'>+</Button>
+        <Button name="0" onClick={handleClick} id='zero'>0</Button>
+        <Button name="." onClick={handleClick} id='decimal'>.</Button>
+        <Button variant="primary" onClick={calculate} id="equals">=</Button>
       </div>
     </div>
   );
